c7333531656c0605895b70a2fc9201ce8c3ce0f5
Fay <69251661+irbala@users.noreply.github.com>
2023-08-24 20:21:36 +0300

    3

--- script.js
+++ script.js
@@ -42,5 +42,15 @@
         observer.observe(element);
     });
 
+    
+    $(document).ready(function() {
+        $('.slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: true
+        });
+    });
 
 
