27d347e98b8e705a3b9ae0e6599f706e50f438c5
Fay <69251661+irbala@users.noreply.github.com>
2023-08-25 06:02:31 +0300

    2

--- script.js
+++ script.js
@@ -67,12 +67,3 @@ $(document).ready(function(){
       } // End if
     });
   });
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.querySelector('nav');
-
-    menuToggle.addEventListener('click', function () {
-        nav.classList.toggle('menu-active');
-    });
-});
